b2dd42a923bee1c4c275a58f82f8a56b5c611307
CHISOM EDOKA <edokachisom48@gmail.com>
2023-09-08 14:20:25 +0100

    added query parameters

--- stage_one/app/main.js
+++ stage_one/app/main.js
@@ -18,16 +18,20 @@ app.listen(PORT, () => {
 });
 
 app.get("/api", (req, res) => {
+  const { track, slack_name } = req.query;
+  if (!track || !slack_name) {
+    res.send("Give a track and slack_name as parameters");
+    return;
+  }
   const query = req.query;
   res.status(200).send({
-    slack_name: "Chisom Edoka",
+    slack_name,
     current_day: days[dayOfWeekIndex],
     utc_time: today,
-    track: "backend",
+    track,
     github_file_url:
       "https://github.com/chizzyedoka/HNG_Internship/blob/master/stage_one/app/main.js",
     github_repo_url: "https://github.com/chizzyedoka/HNG_Internship",
     status_code: "200",
-    query,
   });
 });
