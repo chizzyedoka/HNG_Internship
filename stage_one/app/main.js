53029deaadf229d377add82020837bef92b5a236
CHISOM EDOKA <edokachisom48@gmail.com>
2023-09-10 05:55:56 +0100

    utc

--- stage_one/app/main.js
+++ stage_one/app/main.js
@@ -5,15 +5,22 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 
 require("dotenv").config();
-const today = new Date();
-// const todayFormatted =
+// const today = new Date();
+// console.log(today.getTimezoneOffset());
+// const utcTime =
 //   new Date(today.getTime() + today.getTimezoneOffset() * 60000)
 //     .toISOString()
 //     .split(".")[0] + "Z";
-const todayFormatted =
-  new Date(today.getTime() + today.getTimezoneOffset() * 60000)
+// console.log(utcTime);
+const today = new Date();
+console.log(today.getTimezoneOffset());
+
+const utcTime =
+  new Date(today.getTime() - today.getTimezoneOffset() * 60000)
     .toISOString()
-    .slice(0, -5) + "Z";
+    .split(".")[0] + "Z";
+console.log(utcTime);
+
 const days = [
   "Sunday",
   "Monday",
@@ -38,12 +45,11 @@ app.get("/api", (req, res) => {
   res.status(200).send({
     slack_name,
     current_day: days[dayOfWeekIndex],
-    utc_time: todayFormatted,
+    utc_time: utcTime,
     track,
     github_file_url:
       "https://github.com/chizzyedoka/HNG_Internship/blob/master/stage_one/app/main.js",
     github_repo_url: "https://github.com/chizzyedoka/HNG_Internship",
     status_code: "200",
-    trial: "me",
   });
 });
