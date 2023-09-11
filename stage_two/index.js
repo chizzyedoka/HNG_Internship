89ed2e72be943b516bd2e129cb589e0e47d65802
CHISOM EDOKA <edokachisom48@gmail.com>
2023-09-11 12:09:43 +0100

    connected to MongoDB

--- stage_two/index.js
+++ stage_two/index.js
@@ -1,7 +1,16 @@
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const port = 1000;
 
+mongoose
+  .connect("mongodb://0.0.0.0:27017/hngInternship", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.error("Could not connect to MongoDB...", err));
 //middleware
 app.use(express.json());
 
